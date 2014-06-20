e18ec80c5c71f2f16eb1caf567299c35143420e4
catethysis <catethysis@gmail.com>
2014-06-21 01:14:46 +0400

    prettify function optimized, other regexpes too

--- index.js
+++ index.js
@@ -5,22 +5,13 @@ post_templ = fs.readFileSync('templates/'+post_templ, 'utf8');
 //data=fs.readFileSync('contents/index.yaml', 'utf8');
 //console.log(JSON.stringify(yaml.safeLoad(data), "", 2));
 
-String.prototype.prettify = function(text) {
-	var a = 1;
-	return  this.replace(new RegExp('-', 'g'), '&ndash;')
-				.replace(new RegExp(' &ndash; ', 'g'), '&nbsp;&mdash; ')
-				.replace(new RegExp('&quot;', 'g'), function() {
-					return (a++%2 ? '&laquo;' : '&raquo;');
-				});
-}
+String.prototype.prettify = function() { return this.replace(/ - /g, '&nbsp;&mdash; ').replace(/"([^"]*)"/g, '&laquo;$1&raquo;'); }
 
 processPost = function(path) {
-	post = {};
-	post.data = fs.readFileSync(in_dir+path, 'utf8').split('---');
-	post.meta = yaml.safeLoad(post.data[1]);
-	post.text = marked(post.data[2]).prettify();
+	data = fs.readFileSync(in_dir+path, 'utf8').split('---');
+	post = {meta: yaml.safeLoad(data[1]), text: marked(data[2].prettify())};
 
-	post.html = post_templ.replace(new RegExp('<%title%>', 'g'), post.meta.title).replace('<%content%>', post.text);
+	post.html = post_templ.replace(/<%title%>/g, post.meta.title).replace(/<%content%>/g, post.text);
 	post.name = post.meta.url+'.htm';
 
 	fs.writeFile(out_dir+post.name, post.html);
