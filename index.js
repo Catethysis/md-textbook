ea4e3ac7cdd92dc6d1ffc57c066758e471b06a81
catethysis <catethysis@gmail.com>
2014-06-21 03:16:55 +0400

    Created index_list iterator that processes all md-files in list and accumulate index

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
-var fs=require('fs'), marked=require('marked'), yaml=require('js-yaml'), hljs=require('highlight.js');
+var fs=require('fs'), yaml=require('js-yaml'), marked=require('marked'), hljs=require('highlight.js');
 var post_templ = 'post.us', in_dir = 'contents/', out_dir='production/';
 post_templ = fs.readFileSync('templates/'+post_templ, 'utf8');
 
-//data=fs.readFileSync('contents/index.yaml', 'utf8');
-//console.log(JSON.stringify(yaml.safeLoad(data), "", 2));
+var data=yaml.safeLoad(fs.readFileSync('contents/index.yaml', 'utf8'));
 
 String.prototype.prettify = function() { return this.replace(/ - /g, '&nbsp;&mdash; ').replace(/"([^"]*)"/g, '&laquo;$1&raquo;'); }
 
-processPost = function(path) {
-	data = fs.readFileSync(in_dir+path, 'utf8').split('---');
-	post = {meta: yaml.safeLoad(data[1]), text: marked(data[2].prettify())};
+var processPost = function(path) {
+	var data = fs.readFileSync(in_dir+path+'.md', 'utf8').split('---');
+	var post = {meta: yaml.safeLoad(data[1]), text: marked(data[2].prettify())};
+	console.log('Processing: '+post.meta.title);
 
 	post.html = post_templ.replace(/<%title%>/g, post.meta.title).replace(/<%content%>/g, post.text);
 	post.name = post.meta.url+'.htm';
 
-	fs.writeFile(out_dir+post.name, post.html);
+	fs.writeFileSync(out_dir+post.name, post.html);
+	return {link: post.name, title: post.meta.title};
 }
 
-processPost('1. Старт/PP-OD.md');
+function iterate_index(path, obj, make_level)
+{
+	var index = "";
+	for(var i in obj)
+	{
+		if(typeof obj[i] != 'string')
+			index += Array.isArray(obj[i]) ? i + '\r\n<ol>\r\n' + iterate_index(path + i + '/', obj[i]) + '</ol>\r\n' : iterate_index(path, obj[i]);
+		else
+		{
+			obj[i] = processPost(path + obj[i]);
+			index += '<li>' + obj[i].title + '</li>\r\n';
+		}
+	}
+	return index;
+}
+
+var index = iterate_index('', data.Index);
+
+console.log(JSON.stringify(data, '', 4))
+console.log(index);
