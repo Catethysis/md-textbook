ec70a1860b07a73e51c06d2cce669fd76417a5bd
catethysis <catethysis@gmail.com>
2014-06-21 00:28:23 +0400

    processPost function created

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 var fs=require('fs'), marked=require('marked'), yaml=require('js-yaml'), hljs=require('highlight.js');
+var post_templ = 'post.us', in_dir = 'contents/', out_dir='production/';
+post_templ = fs.readFileSync('templates/'+post_templ, 'utf8');
 
 //data=fs.readFileSync('contents/index.yaml', 'utf8');
 //console.log(JSON.stringify(yaml.safeLoad(data), "", 2));
 
-data = fs.readFileSync('contents/1. Старт/PP-OD.md', 'utf8').split('---');
-meta = yaml.safeLoad(data[1]);
-text = marked(data[2]);
-console.log(meta);
-console.log(text);
+processPost = function(path) {
+	post = {};
+	post.data = fs.readFileSync(in_dir+path, 'utf8').split('---');
+	post.meta = yaml.safeLoad(post.data[1]);
+	post.text = marked(post.data[2]).replace(new RegExp('-', 'g'), '&ndash;').replace(new RegExp(' &ndash; ', 'g'), ' &mdash; ');
+
+	post.html = post_templ.replace(new RegExp('<%title%>', 'g'), post.meta.title).replace('<%content%>', post.text);
+	post.name = post.meta.url+'.htm';
+
+	fs.writeFile(out_dir+post.name, post.html);
+}
+
+processPost('1. Старт/PP-OD.md');
